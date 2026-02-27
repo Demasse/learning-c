aa3bc8d5e9d8c62b9cf25001d785f1d1020f0dfe
Demasse <demassedimas2006@gmail.com>
2026-02-27 16:40:45 +0100

    first commit

--- main.c
+++ main.c
@@ -3,25 +3,12 @@
 
 int main()
 {
-    int a, b,somme,mul,sub;
-    float div;
-    printf("entre le numero de a\n");
-    scanf("%d",&a);
-    printf("entre le numero de b\n");
-    scanf("%d",&b);
-    somme=a+b;
-    mul=a*b;
+     int code=0;
+     while(code!=12345){
+        printf("entre le code ");
+        scanf("%d", &code);
+     }
+     printf("le mdp est correct");
 
-    if(a>b){
-        sub= a-b;
-    }else{
-    sub=b-a;
-    }
-    div=a/b;
-
-    printf("le result de a+b est %d\n", somme);
-    printf("le result de a+b est %d\n", mul);
-    printf("le result de a-b est %d\n", sub);
-    printf("le result de a/b est %d\n", div);
     return 0;
 }
