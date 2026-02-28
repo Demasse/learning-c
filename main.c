96e4a28828ce19ac26d899bab4a9efd5768e7508
Demasse <demassedimas2006@gmail.com>
2026-02-28 15:48:20 +0100

    first commit

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 
 int main()
 {
-    int i,notes[5];
-
-    for(i=0;i<5;i++){
-        printf("entre les nombre\n");
-        scanf("%d", &notes[i]);
-    }
+   int i, notes[5], somme=0 ;
+   float moy;
    for(i=0;i<5;i++){
-        printf(" les nombre sont %d\n", notes[i]);
+    printf("entre les numero\n");
+    scanf("%d", &notes[i]);
+    somme =notes[i] + somme ;
+    moy = (float)somme/5;
+   }
+printf("la sommee est %d\n", somme);
 
-    }
-
+printf("la moyenne est %f\n", moy);
     return 0;
 }
