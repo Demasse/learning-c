2e0024294894e6b144410ba4e88e7244e15a91f2
Demasse <demassedimas2006@gmail.com>
2026-03-03 12:05:01 +0100

    first commit

--- main.c
+++ main.c
@@ -3,19 +3,37 @@
 
 int main()
 {
-  int i, notes[5],somme=0;
-  float moy ;
+  int i,somme=0;
+  float temp[7], moy, max, min;
 
-  for(i=0; i<5; i++){
-    printf("entre les numero\n");
-    scanf("%d", &notes[i]);
-    somme= notes[i] + somme;
-    moy= (float)somme/5;
+  for(i=0; i<7;i++){
+    printf("entre les temperature %d\n", i+1);
+    scanf("%f",&temp[i]);
+    somme= temp[i] + somme ;
+    moy=(float)somme/7;
+
+  }
+    max= temp[0];
+    for(i=0; i<7;i++){
+        if(temp[i] > max){
+            max = temp[i];
+        }
+    }
+
+       min= temp[0];
+    for(i=0; i<7;i++){
+        if(temp[i] < min){
+            min = temp[i];
+        }
+    }
 
 
- }
-   printf("la somme est : %d\n",somme);
-   printf("la moy est : %f\n",moy);
+   printf("la somme total des temp sont : %d\n",somme);
+   printf("la moyenne total des temp sont : %f\n",moy);
+    printf("Temperature la plus bas : %f\n", min);
+    printf("Temperature la plus haute : %f\n", max);
+
+
+
 
 }
-
