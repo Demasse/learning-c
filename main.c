6f667fd7328a19717dfa8cd5e344b352856b906b
Demasse <demassedimas2006@gmail.com>
2026-03-03 16:45:54 +0100

    first commit

--- main.c
+++ main.c
@@ -3,36 +3,15 @@
 
 int main()
 {
-  int i,somme=0;
-  float temp[7], moy, max, min;
+  int age = 15;
+  int *ptr = &age;
 
-  for(i=0; i<7;i++){
-    printf("entre les temperature %d\n", i+1);
-    scanf("%f",&temp[i]);
-    somme= temp[i] + somme ;
-    moy=(float)somme/7;
+  printf("age cest %d\n", age);
+  printf(" adres de age est %p\n", &age);
+    printf(" la valeur stocke est %d\n",ptr);
 
-  }
-    max= temp[0];
-    for(i=0; i<7;i++){
-        if(temp[i] > max){
-            max = temp[i];
-        }
-    }
-
-       min= temp[0];
-    for(i=0; i<7;i++){
-        if(temp[i] < min){
-            min = temp[i];
-        }
-    }
-
-
-   printf("la somme total des temp sont : %d\n",somme);
-   printf("la moyenne total des temp sont : %f\n",moy);
-    printf("Temperature la plus bas : %f\n", min);
-    printf("Temperature la plus haute : %f\n", max);
-
+  *ptr = 25;
+    printf("le nouveaux age est %d\n", age);
 
 
 
